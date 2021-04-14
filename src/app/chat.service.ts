68eb2fc395685e682ae27a1be7958b040752c42a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2021-04-15 00:57:26 +0500

    🎉 Added: Service for chat or Auth

---
+++ src/app/chat.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {ChatMessageModel} from './model/chat-message.model';
+// @ts-ignore
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  user: any;
+  chatMessages: AngularFireList<any>;
+  chatMessage: ChatMessageModel;
+  userName: Observable<string>;
+
+  constructor(private db: AngularFireDatabase, private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(auth => {
+      if (auth !== undefined && auth !== null) {
+        this.user = auth;
+      }
+    });
+  }
+
+
+  sendMessage(msg: string) {
+    const timestamp = this.getTimeStamp();
+    // const email = this.user.email;
+    const email = 'test@gmail.com';
+    this.chatMessages = this.getMessages();
+    this.chatMessages = this.db.list('messages');
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      // userName: this.userName,
+      userName: 'kasa ha jani',
+      email
+    });
+    console.log('Send Message');
+  }
+
+  getMessages() {
+    return this.db.list('messages');
+  }
+
+
+  getTimeStamp() {
+    const now = new Date();
+    const date = now.getUTCFullYear() + '/' + (now.getUTCMonth()) + '/' + now.getUTCDate();
+    const time = now.getUTCHours() + '/' + +(now.getUTCMinutes()) + '/' + now.getUTCSeconds();
+    return (date + ' ' + time);
+  }
+}
