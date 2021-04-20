12879427543a0bc4f9ae6ebf26e8fa33fb63db25
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2021-04-21 02:00:46 +0500

    ♻ update : signup component added for user registration

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import firebase from 'firebase';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import {resolve} from '@angular/compiler-cli/src/ngtsc/file_system';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  email: string;
+  password: string;
+  displayName: string;
+  errorMsg: string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
-  ngOnInit(): void {
+  signUp(): void {
+    const email = this.email;
+    const password = this.password;
+    const displayName = this.displayName;
+    this.authService.signUp(email, password, displayName)
+      .then(resp => this.router.navigate(['chat']))
+      .catch(error => this.errorMsg = error.message);
   }
 
 }
