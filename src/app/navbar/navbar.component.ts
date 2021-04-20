90e7992b52947f094f7b526b68371afef3d7de9d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2021-04-21 01:59:56 +0500

    🎉added : Navbar component added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth.service';
+import {Observable} from 'rxjs';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user: Observable<firebase.User>;
+  userEmail: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
   }
 
+  ngOnInit(): void {
+    this.user = this.authService.authUser();
+    if (this.user === undefined) {
+      console.log('User not Defined');
+      return;
+    }
+    this.user.subscribe(users => {
+      if (users) {
+        this.userEmail = users.email;
+      }
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+
+  login(): void {
+    return;
+  }
 }
